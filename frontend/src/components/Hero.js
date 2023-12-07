7d66f1042005aa257a5705a7b3031f20683f2760
Vincent <106598307+AVincent-Zag@users.noreply.github.com>
2023-12-07 12:30:06 -0700

    edited Hero Component

--- frontend/src/components/Hero.js
+++ frontend/src/components/Hero.js
@@ -1,10 +1,28 @@
+import React from "react";
 import "../style/Hero.css";
+import Typewriter from "./Typewriter";
 
 const Hero = () => {
   return (
-    <div className="introWords">
-      <h1> Welcome to *Bot Name* </h1>
-      <h2> Your loyal dev guide! </h2>
+    <div className="hero-container">
+      <div className="intro-words">
+        <h1>
+          Welcome to <Typewriter text="*Bot Name*" delay= {500} infinite/>
+        </h1>
+        <h2>Your loyal dev assistant!</h2>
+      </div>
+      <div className="card-container">
+        <img
+          src="path_to_your_image.jpg" 
+          alt="Card Image"
+          className="card-image"
+        />
+        <div className="card-content">
+          {/* Add card content here */}
+          <h3>Card Title</h3>
+          <p>Some description about the card.</p>
+        </div>
+      </div>
     </div>
   );
 };
