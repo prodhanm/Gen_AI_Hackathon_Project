83a6d220f93a1bfe8b43d5b41c7bd896601ae8ae
Vincent <106598307+AVincent-Zag@users.noreply.github.com>
2023-12-11 11:00:05 -0700

    edited hero and typewriter components

--- frontend/src/components/Hero.js
+++ frontend/src/components/Hero.js
@@ -1,26 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../style/Hero.css";
 import Typewriter from "./Typewriter";
 
 const Hero = () => {
+  const [cardSentences, setCardSentences] = useState([
+    "Write a 100-character meta description for my blog post about <topic>.",
+    "Write a product description for my [product or service or company]",
+    "Write a list of 5 YouTube video ideas for [your product or company]",
+    "Structure a weekly [newsletter topic] newsletter"
+  ]);
+
+  const [currentSentenceIndex, setCurrentSentenceIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentSentenceIndex((prevIndex) =>
+        (prevIndex + 1) % cardSentences.length
+      );
+    }, 5000); 
+
+    return () => clearInterval(intervalId);
+  }, [cardSentences.length]);
+
   return (
     <div className="hero-container">
       <div className="intro-words">
         <h1>
-          Welcome to <Typewriter text="*Bot Name*" delay= {500} infinite/>
+          Welcome to{" "}
+          <Typewriter text="*Bot Name*" delay={200} infinite />
         </h1>
         <h2>Your loyal dev assistant!</h2>
       </div>
       <div className="card-container">
-        <img
-          src="path_to_your_image.jpg" 
-          alt="Card Image"
-          className="card-image"
-        />
         <div className="card-content">
-          {/* Add card content here */}
-          <h3>Card Title</h3>
-          <p>Some description about the card.</p>
+          <p>
+            <Typewriter
+              text={cardSentences[currentSentenceIndex]}
+              delay={100}
+              infinite
+            />
+          </p>
         </div>
       </div>
     </div>
