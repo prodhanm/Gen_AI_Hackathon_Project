c59bb6a5d2f9903341b971a35d3fcb20214e162d
Sarah Ortega <snortega1999@gmail.com>
2023-12-12 10:23:09 -0500

    Starting on front/back end connection.

--- frontend/src/components/Chat.js
+++ frontend/src/components/Chat.js
@@ -1,21 +1,32 @@
 import "../style/Chat.css";
 import Form from "react-bootstrap/Form";
 import { useState } from 'react'
+import axios from 'axios'
 
 const Chat = () => {
 
   const [textInput, setTextInput] = useState('');
   const [response, setResponse] = useState('');
 
-  const interact = () => {
+  const axiosInstance = axios.create({
+    baseURL: 'http://localhost:5000/',
+  });
+
+  const interact = async () => {
     //sends textInput to the AI and receives the response, then prints the response and clears the text box.
     //the message history should appear above the active text box like a traditional texting app
-    console.log(textInput);
-  };
+    //console.log(textInput);   
+    try {
+      setResponse(await axiosInstance.post('ask-openapi', textInput));
+      return response.data;
+    } catch (error) {
+      console.error('L:', error);
+    }
+  }
 
   return (
     <div className="main">
-      <h1 style={{marginLeft:'3vw'}}> Say Hi to *Bot Name* </h1>
+      <h1 style={{marginLeft:'3vw'}}> Say Hi to Ferestha </h1>
       <div className="chatInterface">
         <div className="convDisplay"></div>
         <div className="input">
