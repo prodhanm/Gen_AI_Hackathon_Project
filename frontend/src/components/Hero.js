02295f632aa1f2c1d65c1bc7e6c1250a68f41651
Vincent <106598307+AVincent-Zag@users.noreply.github.com>
2023-12-12 10:06:59 -0700

    edited hero and typewriter

--- frontend/src/components/Hero.js
+++ frontend/src/components/Hero.js
@@ -1,43 +1,51 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import "../style/Hero.css";
 import Typewriter from "./Typewriter";
 
 const Hero = () => {
   const [cardSentences, setCardSentences] = useState([
-    "Write a 100-character meta description for my blog post about <topic>.",
-    "Write a product description for my [product or service or company]",
-    "Write a list of 5 YouTube video ideas for [your product or company]",
-    "Structure a weekly [newsletter topic] newsletter"
+    " 100-character mention for my product.",
+    " product description for my service.",
+    " list of 5 YouTube video ideas for me",
+    " weekly newsletter topic"
   ]);
-
+  const timeToDisplayEachSentence = 3000;
   const [currentSentenceIndex, setCurrentSentenceIndex] = useState(0);
 
   useEffect(() => {
+    const totalDuration = cardSentences.length * timeToDisplayEachSentence;
     const intervalId = setInterval(() => {
       setCurrentSentenceIndex((prevIndex) =>
         (prevIndex + 1) % cardSentences.length
       );
-    }, 5000); 
+    }, totalDuration);
 
     return () => clearInterval(intervalId);
   }, [cardSentences.length]);
 
+  const handleTypewriterComplete = () => {
+    setCurrentSentenceIndex((prevIndex) => (prevIndex + 1) % cardSentences.length);
+  };
+
   return (
     <div className="hero-container">
-      <div className="intro-words">
-        <h1>
-          Welcome to{" "}
-          <Typewriter text="*Bot Name*" delay={200} infinite />
-        </h1>
-        <h2>Your loyal dev assistant!</h2>
+      <div className="intro-card">
+        <div className="intro-words">
+          <h1>
+            Welcome to Ferestha
+          </h1>
+          <h2>Your loyal dev assistant!</h2>
+        </div>
       </div>
       <div className="card-container">
         <div className="card-content">
           <p>
+            Structure a 
             <Typewriter
               text={cardSentences[currentSentenceIndex]}
               delay={100}
-              infinite
+              infinite={true}
+              onAnimationComplete={handleTypewriterComplete}
             />
           </p>
         </div>
--- frontend/src/components/Typewriter.js
+++ frontend/src/components/Typewriter.js
@@ -1,24 +1,29 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
-const Typewriter = ({ text, delay, infinite }) => {
-  const [currentText, setCurrentText] = useState('');
+const Typewriter = ({ text, delay, infinite, onAnimationComplete }) => {
+  const initialText = ' '.repeat(text.length);
+  const [currentText, setCurrentText] = useState(initialText);
   const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
     let timeout;
 
-    if (currentIndex < text.length) { // Change this condition
+    if (currentIndex < text.length) {
       timeout = setTimeout(() => {
-        setCurrentText(prevText => prevText + text[currentIndex]);
-        setCurrentIndex(prevIndex => prevIndex + 1);
+        setCurrentText((prevText) =>
+          prevText.substring(0, currentIndex) + text[currentIndex] + prevText.substring(currentIndex + 1)
+        );
+        setCurrentIndex((prevIndex) => prevIndex + 1);
       }, delay);
     } else if (infinite) {
       setCurrentIndex(0);
-      setCurrentText('');
+      setCurrentText(initialText);
+    } else if (onAnimationComplete) {
+      onAnimationComplete();
     }
 
     return () => clearTimeout(timeout);
-  }, [currentIndex, delay, infinite, text]);
+  }, [currentIndex, delay, infinite, text, onAnimationComplete, initialText]);
 
   return <span>{currentText}</span>;
 };
